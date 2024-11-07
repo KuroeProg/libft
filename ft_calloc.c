14f12a6443b5f59ee2b6e3eee927596fec11952b
root <itsumy81@gmail.com>
2024-11-07 14:02:24 +0100

    calloc updated

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: cfiachet <cfiachet@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 16:48:41 by cfiachet          #+#    #+#             */
-/*   Updated: 2024/10/13 16:50:20 by cfiachet         ###   ########.fr       */
+/*   Updated: 2024/11/07 13:51:35 by cfiachet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,11 @@
 void	*ft_calloc(size_t num, size_t size)
 {
 	void	*tab;
+	size_t	total;
 
+	total = num * size;
+	if (num != 0 && total / num != size)
+		return (NULL);
 	tab = malloc(num * size);
 	if (tab == NULL)
 		return (tab);
--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: cfiachet <cfiachet@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 19:01:56 by cfiachet          #+#    #+#             */
-/*   Updated: 2024/11/04 20:17:09 by cfiachet         ###   ########.fr       */
+/*   Updated: 2024/11/06 15:02:31 by cfiachet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		}
 		new_elem = ft_lstnew(temp);
 		if (!new_elem)
-		return (del(temp), ft_lstclear(&current, del), NULL);
+			return (del(temp), ft_lstclear(&current, del), NULL);
 		ft_lstadd_back(&current, new_elem);
 		lst = lst->next;
 	}
